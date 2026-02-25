7cb39622ad507b902d6e43d91061dbe6b96f256f
jwookie2 <jwookie2@hotmail.com>
2026-02-25 13:02:34 -0500

    Refactor server.js and add new API endpoints
    Refactored server.js to improve structure and added new endpoints for user registration and credit purchasing.

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,75 @@
+const express=require("express");
+const cors=require("cors");
+const {v4:uuidv4}=require("uuid");
 
-const express=require("express")
+const db=require("./database");
+const tasks=require("./tasks");
+const {createToken,verifyToken}=require("./auth");
+const {createPayment}=require("./stripe");
 
-const cors=require("cors")
+const app=express();
 
-const app=express()
-
-app.use(cors())
-
-app.use(express.json())
-
-
-app.get("/",(req,res)=>{
-
-res.send("Horse Pacer API")
-
-})
+app.use(cors());
+app.use(express.json());
 
 
-app.listen(5000,()=>{
 
-console.log("Server running")
+app.post("/register",(req,res)=>{
 
-})
+const id=uuidv4();
+
+db.run(
+
+"INSERT INTO users VALUES(?,?,?,?)",
+
+[id,req.body.name,req.body.email,20]
+
+);
+
+res.json({
+token:createToken({id})
+});
+
+});
+
+
+app.get("/tasks",(req,res)=>{
+
+tasks.getTasks((err,data)=>{
+
+res.json(data);
+
+});
+
+});
+
+
+app.get("/steps/:taskId",verifyToken,(req,res)=>{
+
+tasks.getSteps(req.params.taskId,
+
+(err,data)=>{
+
+res.json(data);
+
+});
+
+});
+
+
+app.post("/buyCredits",async(req,res)=>{
+
+const payment=
+
+await createPayment(req.body.amount);
+
+res.json(payment);
+
+});
+
+
+app.listen(3000,()=>{
+
+console.log("HORSE PACER SaaS Running");
+
+});
